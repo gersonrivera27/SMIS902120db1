24b44bdc0939c40a974f45350e5564bf2d16ad6f
gersonrivera27 <gersonrivera02@hotmail.com>
2021-10-04 10:34:06 -0600

    Se actualizo la vista

--- Uniones y vistas.sql
+++ Uniones y vistas.sql
@@ -19,8 +19,37 @@ INNER JOIN Categories c
 ON p.CategoryID = c.CategoryID
 INNER JOIN Suppliers s
 ON p.SupplierID = s.SupplierID
-
+--consultar vistas
 SELECT 
 * FROM listadoProductos
+--MODIFICAR LA VISTA ANTERIOR
+AlTER VIEW listadoProductos AS
+SELECT p.ProductName Producto,
+c.CategoryName Categoria,
+s.ProductID ID
+FROM Products p
+RIGHT JOIN Categories c
+ON p.CategoryID = c.CategoryID
+LEFT JOIN Suppliers o
+ON p.SupplierID = o.SupplierID
+RIGHT JOIN [Order Details] s
+ON p.ProductID = s.ProductID
 
+SELECT *
+FROM listadoProductos
 
+--Modificar con FUllJoin 
+AlTER VIEW listadoProductos AS
+SELECT p.ProductName Producto,
+c.CategoryName Categoria,
+s.ProductID ID
+FROM Products p
+FULL JOIN Categories c
+ON p.CategoryID = c.CategoryID
+FULL JOIN Suppliers o
+ON p.SupplierID = o.SupplierID
+FULL JOIN [Order Details] s
+ON p.ProductID = s.ProductID
+
+SELECT * 
+FROM listadoProductos
