3f54c5147dc0c7071ba8c2f02ce244fcc7c83ae3
gersonrivera27 <gersonrivera02@hotmail.com>
2021-10-04 10:01:32 -0600

    Se ha agregado la vista

--- Uniones y vistas.sql
+++ Uniones y vistas.sql
@@ -1,3 +1,26 @@
 use Northwind
 
+SELECT p.ProductName producto, 
+c.CategoryName categoria
+FROM Products p 
+INNER JOIN Categories c
+ON p.CategoryID = c.CategoryID
+
+
+
+--Ejercicio
+
+CREATE VIEW  listadoProductos AS
+SELECT p.ProductName Productos,
+c.CategoryID Categorias,
+s.ContactName Proveedor
+FROM Products p
+INNER JOIN Categories c
+ON p.CategoryID = c.CategoryID
+INNER JOIN Suppliers s
+ON p.SupplierID = s.SupplierID
+
+SELECT 
+* FROM listadoProductos
+
 
